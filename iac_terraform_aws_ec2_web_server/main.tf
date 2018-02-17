a0e9de25b5966bce0f734205d6f6b6818321ba71
Barath <barathmacdec91@gmail.com>
2018-02-17 19:07:40 +0530

    polishing readme

--- iac_terraform_aws_ec2_web_server/main.tf
+++ iac_terraform_aws_ec2_web_server/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web-server" {
   ami = "${var.ami}"
   instance_type = "${var.instance_type}"
   key_name = "${var.key_pair_name}"
-  security_groups = ["${aws_security_group.default_allow_all_sg.id}"]
+  security_groups = ["${aws_security_group.default_allow_all_sg.name}"]
   tags {
     Name = "web-server"
   }

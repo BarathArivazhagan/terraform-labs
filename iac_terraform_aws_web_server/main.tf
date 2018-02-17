3cc35f4ef599f82049cfb58e747821367b25f8ff
Barath <barathmacdec91@gmail.com>
2018-02-17 17:39:21 +0530

    added aws_web_server_terraform scripts

---
+++ iac_terraform_aws_web_server/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web-server" {
+  ami = "ami-4bf3d731"
+  instance_type = "t2.micro"
+  key_name = "barath_mac_pair"
+  
+  tags {
+    Name = "web-server"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "allow_all"
+  }
+}

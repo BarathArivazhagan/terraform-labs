bfaa45f56198726a9ef54ffd530f029ee7952b93
Barath <barathmacdec91@gmail.com>
2018-02-18 10:10:47 +0530

    fix sg issue

--- iac_terraform_aws_ec2_web_server/main.tf
+++ iac_terraform_aws_ec2_web_server/main.tf
@@ -28,7 +28,6 @@ resource "aws_security_group" "default_allow_all_sg" {
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = ["pl-12c4e678"]
   }
 
   tags {

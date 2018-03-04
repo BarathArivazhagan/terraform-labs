9ff17b308c29a3cad0851f3612dc952451239311
Barath Arivazhagan <barathmacdec91@gmail.com>
2018-03-04 10:19:19 +0530

    added sonarqube module

---
+++ iac_terraform_aws_sonarqube/main.tf
@@ -0,0 +1,112 @@
+provider "aws" {
+  region = "${var.aws_region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+
+
+}
+
+
+resource "aws_db_instance" "sonarqube-db" {
+  name = "${var.sonar_database_name}"
+  instance_class = "db.t2.micro"
+  identifier = "${var.sonar_database_name}"
+  vpc_security_group_ids = ["${aws_security_group.rds_sonarqube_security_group.id}"]
+  db_subnet_group_name = "${var.db_subnet_group_name}"
+  allocated_storage = "${var.db_allocated_storage}"
+  engine = "mysql"
+  engine_version       = "5.6.17"
+  parameter_group_name = "default.mysql5.6"
+  username = "${var.sonar_master_username}"
+  password = "${var.sonar_master_password}"
+  port = 3306
+
+}
+
+output "sonarqube_db_rds_endpoint" {
+  value = "${aws_db_instance.sonarqube-db.endpoint}"
+}
+
+
+
+resource "aws_security_group" "rds_sonarqube_security_group" {
+
+  name        = "${var.rds_db_security_group_name}"
+  description = "RDS security group for mysql database engine"
+  vpc_id = "${var.vpc_id}"
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.rds_db_security_group_name}"
+  }
+
+
+}
+
+
+# This resource block creates AWS EC2 instance with the details provided
+resource "aws_instance" "sonarqube-instance" {
+
+  ami = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_pair_name}"
+  vpc_security_group_ids = ["${aws_security_group.default_allow_all_sg.id}"]
+  subnet_id = "${var.subnet_id}"
+  tags {
+    Name = "sonarqube-instance"
+  }
+
+  depends_on = ["aws_db_instance.sonarqube-db"]
+  # chef provisioner configuration
+  provisioner "chef" {
+    node_name = "${var.node_name}"
+    server_url = "${var.chef_server_url}"
+    user_key = "${file(var.chef_user_key_filepath)}"
+    user_name = "${var.chef_user_name}"
+    run_list = "${var.chef_run_list}"
+    environment = "${var.chef_environment}"
+    recreate_client = true
+
+    connection {
+      type = "ssh"
+      user = "${var.ssh_connection_user}"
+      private_key = "${file(var.chef_client_private_key)}"
+    }
+  }
+}
+
+# This resource block creates AWS Secutiry Group within VPC ID with the details provided
+resource "aws_security_group" "default_allow_all_sg" {
+  name        = "default_allow_all_sg"
+  description = "Allow all inbound traffic"
+  vpc_id = "${var.vpc_id}"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "allow_all"
+  }
+}
---
+++ iac_terraform_aws_sonarqube/variables.tf
@@ -0,0 +1,98 @@
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  description = "AWS access key to access AWS resources"
+  default = "" # Not recommended to use access key as it leads to security issues
+}
+
+
+variable "aws_secret_key" {
+  description = "AWS secret key to access AWS resources"
+  default = "" # Not recommended to use secret key as it leads to security issues
+}
+
+variable "vpc_id"{
+  description = "AWS VPC ID to be associated with the instance"
+  default = ""
+}
+
+variable "subnet_id" {
+  description = "subnet id within VPC network to be associated with the instance"
+  default = ""
+}
+
+variable "key_pair_name"{
+  description = "key pair to be associated with the instance"
+}
+
+variable "instance_type" {
+
+  description = "instance_type to be associated with the instance"
+  default = "t2.micro" # use t2 micro free tier as default instance type
+}
+
+variable "ami" {
+  description = "ami to be associated with the instance"
+  default = "ami-97785bed" # use amazon linux ami as default (Amazon Linux AMI 2017.09.1 (HVM), SSD Volume Type)
+}
+
+variable "instance_tags" {
+   type = "map"
+   default = {
+    Name = "web-server"
+  }
+}
+
+variable "chef_user_key_filepath" {}
+
+variable "node_name" {
+  description = "node name to be associated with the instance in chef server"
+}
+
+variable "chef_user_name" {
+   description = "chef server user name "
+}
+variable "chef_server_url" {
+
+   description = "chef server url "
+}
+
+variable "chef_client_private_key" {
+  description = "chef client/chef node instance key pair to login in to the instance using ssh"
+}
+
+variable "chef_run_list" {
+  type = "list"
+  description = "describe the run list to be executed in the chef node instance"
+}
+
+variable "chef_environment" {
+  description = "environmnet to pass for chef client run"
+}
+
+variable "ssh_connection_user" {
+  description = "ssh connection user name to login to the chef node/chef client instance"
+}
+
+variable "sonar_database_name" {
+
+  description = "RDS database name"
+}
+
+variable "sonar_master_username" {
+  description = "RDS user name "
+}
+
+variable "sonar_master_password" {
+  description = "RDS user password "
+}
+
+variable "db_allocated_storage" {}
+
+variable "rds_db_security_group_name" {}
+
+variable "db_subnet_group_name" {}

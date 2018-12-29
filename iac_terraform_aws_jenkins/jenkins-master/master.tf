1007c4113aa7b98848dbb9d74964490f10c14fc6
BarathArivazhagan <barathmacdec91@gmail.com>
2018-12-29 20:57:53 +0530

    modified ELB to LB of network type

--- iac_terraform_aws_jenkins/jenkins-master/master.tf
+++ iac_terraform_aws_jenkins/jenkins-master/master.tf
@@ -50,28 +50,8 @@ resource "aws_lb" "jenkins_master_lb" {
   security_groups = ["${var.master_lb_sg}"]
   subnets         = "${var.master_lb_subnets}"
 
-  listener {
-    instance_port      = 8080
-    instance_protocol  = "http"
-    lb_port            = 443
-    lb_protocol        = "https"
-    ssl_certificate_id = "${var.master_lb_ssl_cert}"
-  }
-
-  health_check {
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    timeout             = 5
-    target              = "TCP:8080"
-    interval            = 30
-  }
-
-  instances                   = ["${aws_instance.jenkins_master_instance.id}"]
-  cross_zone_load_balancing   = true
   internal                    = false
-  idle_timeout                = 400
-  connection_draining         = true
-  connection_draining_timeout = 400
+
 
   tags {
     Name = "${var.master_lb_name}"

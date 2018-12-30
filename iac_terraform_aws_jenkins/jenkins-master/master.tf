9fef0ba3373a0cc963d60d37988594176909b2a2
BarathArivazhagan <barathmacdec91@gmail.com>
2018-12-30 13:55:50 +0530

    fix aws lb attachment issues

--- iac_terraform_aws_jenkins/jenkins-master/master.tf
+++ iac_terraform_aws_jenkins/jenkins-master/master.tf
@@ -85,12 +85,25 @@ resource "aws_lb_listener" "jenkins_lb_listener" {
 
   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
   certificate_arn   = "${var.master_lb_ssl_cert}"
+  protocol = "TCP"
   "default_action" {
     type = "forward"
     target_group_arn = "${aws_lb_target_group.jenkins_lb_target_group.arn}"
   }
   load_balancer_arn = "${aws_lb.jenkins_master_lb.arn}"
   port = 443
+}
+
+resource "aws_lb_listener_rule" "jenkins_lb_listener_rule" {
+  listener_arn = "${aws_lb_listener.jenkins_lb_listener.arn}"
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.jenkins_lb_target_group.arn}"
+  }
+
+
 }
 
 
